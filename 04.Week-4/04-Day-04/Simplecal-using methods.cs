41c4ad0215a8c1678381c5a92e5dff674b4b02d3
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-03-16 07:09:37 +0530

    Add files via upload

---
+++ 04.Week-4/04-Day-04/Simplecal-using methods.cs
@@ -0,0 +1,35 @@
+﻿namespace Day_19
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int num1;
+
+                Console.WriteLine("enter fst no:");
+                num1 = int.Parse(Console.ReadLine());
+
+               
+
+
+                Calculator calobj = new Calculator();
+
+                int addition = calobj.Add(num1,5);
+                Console.WriteLine($"Addition:  {addition}");
+                Console.WriteLine($"Subtraction:  {calobj.Subtract(num1, 5)}");
+
+
+            }
+        }
+    }
+}
