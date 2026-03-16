3816f29ab064b1bb4e21e23db92d985c1f888350
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-03-16 19:33:04 +0530

    Add files via upload

---
+++ 05-Week-5/05-Day-01/Bank-Account.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp1
+{
+    class BankAccount
+    {
+
+        private string accountNumber;
+        private double balance;
+
+        public string AccountNumber=>accountNumber;
+        
+        public double Balance =>  balance; 
+        
+
+
+        public BankAccount(string AccNo){
+            accountNumber=AccNo;
+            balance=0;
+        }
+
+        public void deposit(double amount)
+        {
+            if( amount <=0){
+                Console.WriteLine("Invalid deposit amnt");
+                return;
+            }
+            balance += amount;
+            Console.WriteLine("amnt deposit:"+ amount);
+            Console.WriteLine("amnt blnce:" + balance);
+
+        }
+
+        public void Withdraw(double amount)  {
+            if( amount <=0){
+                Console.WriteLine("Invalid withdrawal amount");
+                return;
+            }
+
+            if (amount > balance)
+            {
+                Console.WriteLine("Insufficient Balance");
+                return;
+            }
+
+            balance = balance - amount;
+            Console.WriteLine("Amount Withdrawn: " + amount);
+            Console.WriteLine("Current Balance = " + balance);
+        }
+
+    }
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("S123");
+
+
+            Console.Write("Enter Deposit Amount: ");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            account.deposit(deposit);
+
+            Console.Write("Enter Withdraw Amount: ");
+            double withdraw = Convert.ToDouble(Console.ReadLine());
+            account.Withdraw(withdraw);
+
+            Console.WriteLine("Final Balance = " + account.Balance);
+
+        }
+    }
+}
---
+++ 05-Week-5/05-Day-01/Employee-salary.cs
@@ -0,0 +1,56 @@
+﻿namespace Employee_salary
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            BaseSalary = salary;
+        }
+        public virtual double CalculateSalary()
+        { return BaseSalary; }
+    }
+
+
+    class Manager: Employee
+    {
+        public Manager(string name,double salary):base(name,salary) {
+        
+        }
+        public override double CalculateSalary() {
+            return BaseSalary+(BaseSalary*0.20);
+        }
+    }
+    class Developer : Employee
+    {
+        public Developer(string name, double salary) : base(name, salary)
+        {
+        }
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.10);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Base Salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            // Polymorphism using base class reference
+            Employee manager = new Manager("Manager", salary);
+            Employee developer = new Developer("Developer", salary);
+
+            Console.WriteLine("Manager Salary = " + manager.CalculateSalary());
+            Console.WriteLine("Developer Salary = " + developer.CalculateSalary());
+        
+
+    }
+    }
+}
