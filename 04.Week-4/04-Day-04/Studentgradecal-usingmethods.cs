a04a485bf8ccb8e0c4c2437a6008ac88b3109690
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-03-16 07:10:19 +0530

    Add files via upload

---
+++ 04.Week-4/04-Day-04/Studentgradecal-usingmethods.cs
@@ -0,0 +1,41 @@
+﻿namespace Student_grade_methods
+{
+    class Student {
+
+        public double CalAvg(int m1, int m2,int m3)
+        {
+            double avg = (m1 + m2 + m3) / 3.0;
+            return avg;
+        }
+    internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int m1,m2,m3;
+                Console.WriteLine("Enter marks 1:");
+                m1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter marks 2:");
+                m2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter marks 3:");
+                m3 = int.Parse(Console.ReadLine());
+
+                Student stuObj = new Student();
+
+                double average=stuObj.CalAvg(m1,m2,m3);
+
+                Console.WriteLine($"Average {average}");
+                if (average >= 80)
+                    Console.WriteLine("Grade = A");
+                else if (average >= 60)
+                    Console.WriteLine("Grade = B");
+                else if (average >= 40)
+                    Console.WriteLine("Grade = C");
+                else
+                    Console.WriteLine("Grade = F");
+
+            }
+        }
+    }
+}
