01a419b8e14bae1ee23ab0d66484a6978fa0ee7e
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-03-29 20:30:19 +0530

    Add files via upload

---
+++ 06-Week-6/06-Day-04/Controller/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+namespace WebApplication3.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Login Page
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        // POST: Handle Login
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Username == "sahithi" && model.Password == "1234")
+            {
+                ViewBag.Message = "Login Successful!";
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Username or Password";
+                return View();
+            }
+        }
+    }
+}
---
+++ 06-Week-6/06-Day-04/Controller/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
