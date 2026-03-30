7a15c29ee4ef817c0f42fef0c34cef4f833e6845
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-03-30 15:39:52 +0530

    Add files via upload

---
+++ 06-Week-6/06-Day-05/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        [Required(ErrorMessage = "Product Id is required")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be 5 to 15 characters")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category must be 5 to 15 characters")]
+        public string Category { get; set; }
+    }
+}
