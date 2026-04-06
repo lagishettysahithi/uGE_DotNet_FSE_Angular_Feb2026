f30899daff43f909a206b3fa598feb395b5a8977
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-04-06 09:46:27 +0530

    Add files via upload

---
+++ 07-Week-7/07-Day-03/Services/ContactServices.cs
@@ -0,0 +1,24 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Service
+{
+    public class ContactService : IContactService
+    {
+        private static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public List<ContactInfo> GetAllContacts()
+        {
+            return contacts;
+        }
+
+        public ContactInfo GetContactById(int id)
+        {
+            return contacts.FirstOrDefault(c => c.ContactId == id);
+        }
+
+        public void AddContact(ContactInfo contact)
+        {
+            contacts.Add(contact);
+        }
+    }
+}
---
+++ 07-Week-7/07-Day-03/Services/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Service
+{
+    public interface IContactService
+    {
+        List<ContactInfo> GetAllContacts();
+        ContactInfo GetContactById(int id);
+        void AddContact(ContactInfo contact);
+    }
+}
