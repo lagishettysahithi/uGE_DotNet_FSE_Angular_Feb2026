6f8f2e9d164ee25326f524614b045b516359d731
lagishettysahithi <lagishettysahithi90@gmail.com>
2026-04-07 17:37:17 +0530

    Delete 08-Week-8/08-Day-01/Models directory

--- 08-Week-8/08-Day-01/Models/Company.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication2.Models
-{
-    public class Company
-    {
-        public int CompanyId { get; set; }
-        public string CompanyName { get; set; } = string.Empty;
-    }
-}
